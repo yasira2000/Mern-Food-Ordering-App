7a8babb9d2e9c01d4d7f371c75c1e9daebbff687
Yasira Punsith <yasirapunsith@gmail.com>
2024-06-20 23:13:47 +0530

    Finished building responsive home page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>HOME PAGE</Layout>}></Route>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      ></Route>
       <Route
         path="/user-profile"
         element={<span>USER PROFILE PAGE</span>}
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 type Props = {
   children: React.ReactNode;
@@ -12,6 +13,7 @@ function Layout({ children }: Props) {
       <Header />
       <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 }
