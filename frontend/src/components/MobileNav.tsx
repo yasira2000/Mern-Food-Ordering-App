efd2ec52558cd25bccf960a7eff16943d7c36279
Yasira Punsith <yasirapunsith@gmail.com>
2024-06-21 22:36:42 +0530

    Add login logout functionalities for mobile devices

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -6,10 +6,15 @@ import {
   SheetDescription,
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import MobileNavLinks from "./MobileNavLinks";
 
 export default function MobileNav() {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -17,11 +22,29 @@ export default function MobileNav() {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          <span>Welcome to MernEats.com</span>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Weclome to MernEats.com!</span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => {
+                loginWithRedirect();
+              }}
+              className="flex-1 font-bold bg-orange-500"
+            >
+              Log In
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
