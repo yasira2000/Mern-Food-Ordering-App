d47a5110aff7c3a93fce8e31ecc918e232f3d84c
Yasira Punsith <yasirapunsith@gmail.com>
2024-06-21 22:06:45 +0530

    Add login logout functionality and drowdown component for them for MainNav

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,14 +1,23 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 export default function MainNav() {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    <div>
-      <Button
-        variant="ghost"
-        className="font-bold hover:text-orange-500 hover:bg-white"
-      >
-        Log In
-      </Button>
-    </div>
+    <span>
+      {isAuthenticated ? (
+        <UsernameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </span>
   );
 }
